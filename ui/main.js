6daca54e732fa2449568bf13f051dfdd328a2251
vinuviranyu <srinivas.meena@gmail.com>
2017-02-05 17:15:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+            Hi! I am Gurunathan Srinivasan and this is my first app. This programme is to test the capabilti
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
