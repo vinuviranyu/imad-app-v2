dbdd63e6159b6daa55b0e8c61fbe7c8bb302c891
vinuviranyu <srinivas.meena@gmail.com>
2017-02-13 21:58:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,16 @@
-<doctype html>
+<!doctype html>
 <html>
     <head>
         <link href="/ui/style.css" rel="stylesheet" />
     </head>
     <body>
         <div class="center">
-            <img src="/ui/madi.png" class="img-medium"/>
+            <img src="/ui/madi.png" class="img-large"/>
         </div>
         <br>
         <div class="center text-big bold">
             Hi! I am Gurunathan Srinivasan and this is my first app. I am a great man and am IIT Material. 
-            Hopeful for IIT Madras for Computer Science or Aerospace or Mechanical Engineering.
+            <H3>Hopeful for IIT Madras for Computer Science or Aerospace or Mechanical Engineering.</H3>
         </div>
         <script type="text/javascript" src="/ui/main.js">
         </script>
